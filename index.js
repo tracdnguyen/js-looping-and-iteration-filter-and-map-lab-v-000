7f772986ece0cd71a75fd060935b94989f07dfd2
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-28 17:04:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,2 @@
 // Code your solution here:
-function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter(function(callback) {
-    return callback.revenue > revenue;
-  });
-}
 
-function driverNamesWithRevenueOver(drivers, revenue) {
-  return driversWithRevenueOver(drivers, revenue).map(function(callback) {
-    return callback.name
-  })
-}
-
-function exactMatch(drivers, driverObj) {
-  return drivers.filter(function(callback) {
-    let match = [];
-
-    for (const element in driverObj) {
-      match = callback[element] === driverObj[element];
-    }
-    return match;
-  })
-}
-
-function exactMatchToList(drivers, driverObj) {
-  return exactMatch(drivers, driverObj).map(function(driver) {
-    return driver.name;
-  })
-}
